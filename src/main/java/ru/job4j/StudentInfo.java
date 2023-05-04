641d8a3b04673a35ca1a9e215f074529c6852893
RomanKolyagin <romanderyabin@mail.ru>
2023-05-04 17:06:32 +0800

    4.1. Разбор первой программы. [#159033 #231220]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Roman Kolyagin");
+        System.out.println("15.11.1997");
+    }
+}
