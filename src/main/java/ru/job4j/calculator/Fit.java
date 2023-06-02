792dfc87c8c7db47d90800d3f0ace893ce610966
RomanKolyagin <romanderyabin@mail.ru>
2023-06-03 03:31:29 +0800

    6. Идеальный вес [#41584 #233500]

---
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -0,0 +1,22 @@
+package ru.job4j.calculator;
+
+public class Fit {
+
+    public static double manWeight(short height) {
+        return (height - 100) * 1.15;
+    }
+
+    public static double womanWeight(short height) {
+        return (height - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+        short height = 187;
+        double man = manWeight(height);
+        System.out.println("Man 187 is " + man);
+
+        height = 155;
+        double women = womanWeight(height);
+        System.out.println("Women 164 is " + women);
+    }
+}
