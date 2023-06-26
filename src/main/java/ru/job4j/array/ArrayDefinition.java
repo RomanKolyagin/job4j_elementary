0b2b6d5d2afb58056080ef129f618486a6cfa52c
RomanKolyagin <romanderyabin@mail.ru>
2023-06-26 22:25:55 +0800

    6.0.3. Заполнение массива. [#226300 #287158]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "Petr Arsentev";
+        names[1] = "Roman Kolyagin";
+        names[2] = "Andre Dovakin";
+        names[3] = "Alina Sharon";
 
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
