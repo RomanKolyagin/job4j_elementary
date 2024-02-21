acf47f5d2027766633de5b0bde892602558fb286
RomanKolyagin <romanderyabin@mail.ru>
2024-02-21 19:39:46 +0800

    6.1. Классический поиск перебором. [#33489 #288413]

---
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class FindLoopTest {
+
+    @Test
+    void whenArrayHas5Then0() {
+        int[] data = new int[] {5, 10, 3};
+        int element = 5;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 0;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas12ThenMinus1() {
+        int[] data = new int[] {5, 10, 3};
+        int element = 12;
+        int result = FindLoop.indexOf(data, element);
+        int expected = -1;
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void whenArrayHas6Then3() {
+        int[] data = new int[] {1, 2, 4, 6};
+        int element = 6;
+        int result = FindLoop.indexOf(data, element);
+        int expected = 3;
+        assertThat(result).isEqualTo(expected);
+    }
+}
