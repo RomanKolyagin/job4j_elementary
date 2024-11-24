dfd2bf020636d6ec1c35846c1e3fb1e314c3a6bf
DarkSide <romanderyabin@mail.ru>
2024-11-24 15:54:35 +0800

    1.1. Что такое import [#504773 #260388]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,25 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double minusAndDivided(double first, double second) {
+        return minus(first, second)
+                + divided(first, second);
+    }
+
+    public static double minusAndPlusAndMultiplyAndDivided(double first, double second) {
+        return minus(first, second) + divided(first, second)
+                + multiply(first, second) + divided(second, first);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+    }
+}
