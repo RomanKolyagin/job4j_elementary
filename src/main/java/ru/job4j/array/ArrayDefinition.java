d1be2bcdd02c3ba9446bed4424a0717a52b9f502
DarkSide <romanderyabin@mail.ru>
2024-12-05 04:58:57 +0800

    6.0.4. Массивы и цикл for. [#226665 #287160]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -15,9 +15,8 @@ public class ArrayDefinition {
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
-        System.out.println(names[0]);
-        System.out.println(names[1]);
-        System.out.println(names[2]);
-        System.out.println(names[3]);
+        for (String name : names) {
+            System.out.println(name);
+        }
     }
 }
