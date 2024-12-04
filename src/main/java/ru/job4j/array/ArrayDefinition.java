6d2d0ebf65bb8940ef310a06774bea5dd9aac32e
DarkSide <romanderyabin@mail.ru>
2024-12-05 04:49:28 +0800

    6.0.3. Заполнение массива. [#226300 #287158]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -6,9 +6,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[1000];
         float[] prices = new float[40];
+        String[] names = new String[4];
+        names[0] = "John";
+        names[1] = "Jane";
+        names[2] = "Doe";
+        names[3] = "Bob";
 
         System.out.println(ages.length);
         System.out.println(surnames.length);
         System.out.println(prices.length);
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
