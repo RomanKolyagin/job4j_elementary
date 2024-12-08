9d7e6c7d24c18aef173bc523eee6624361862a33
DarkSide <romanderyabin@mail.ru>
2024-12-08 12:00:46 +0800

    6.6.0. Двумерный массив [#226796 #297492]

---
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class Sequence {
+
+    public static void main(String[] args) {
+        int[][] array = new int[3][3];
+        array[0][0] = 1;
+        array[0][1] = 2;
+        array[0][2] = 3;
+        array[1][0] = 4;
+        array[0][1] = 5;
+        array[0][2] = 6;
+        array[2][0] = 7;
+        array[0][1] = 8;
+        array[0][2] = 9;
+    }
+}
