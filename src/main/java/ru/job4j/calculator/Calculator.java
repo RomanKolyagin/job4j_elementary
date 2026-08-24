232c9b6f12dd8ddf3da0c37d8b63bc091e194cd9
RomanKolyagin <romanderyabin@mail.ru>
2026-08-24 14:43:38 +0800

    4. Методы [#231984 #232916]

--- src/main/java/ru/job4j/calculator/Calculator.java
+++ src/main/java/ru/job4j/calculator/Calculator.java
@@ -2,10 +2,14 @@ package ru.job4j.calculator;
 
 public class Calculator {
 
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
+    }
+
     public static void main(String[] args) {
-        int size = 10;
-        size = 100;
-        size = size - 5;
-        System.out.println(size);
+        plus(100, 500);
+        plus(4, 2);
+        plus(3, 5);
     }
 }
