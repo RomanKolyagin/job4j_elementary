93181a79f3e76d23e2af391ed01478365cb87d57
RomanKolyagin <romanderyabin@mail.ru>
2026-08-26 11:24:57 +0800

    9.1. String [#237407 #259393]

--- src/main/java/ru/job4j/condition/Greeting.java
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -5,9 +5,9 @@ public class Greeting {
     public static void main(String[] args) {
         String idea = "I like Java!";
         System.out.println(idea);
-        idea = idea + " But i am a newbie.";
+        idea = idea + " But i am a newbie. ";
         int year = 2026;
-        idea = idea + year;
+        idea += year;
         System.out.println(idea);
     }
 }
